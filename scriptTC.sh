bf0404c222ccc99553de22613cbe96c192c0641d
hanula1 <91565777+hanula1@users.noreply.github.com>
2024-10-25 09:53:13 +0200

    Szkic plików

---
+++ scriptTC.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+INTERFACE="enp0s8"
+RATE="100mbit"         # Całkowita przepustowość interfejsu
+HTTP_RATE="70mbit"     # 70% dla HTTP/HTTPS
+MAIL_RATE="25mbit"     # 25% dla SMTP/POP3/IMAP
+ICMP_RATE="5mbit"      # 5% dla ICMP
+
